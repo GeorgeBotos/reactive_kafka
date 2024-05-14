55dacb8eb8e716de558d8f6b0a375093b335df5a
george.botos.no1@gmail.com <george.botos.no1@gmail.com>
2024-05-14 21:36:02 +0100

    sec5, multiple consumer with multiple partitions

---
+++ src/main/java/com/rm_higs/reactive_kafka/sec5/KafkaConsumer.java
@@ -0,0 +1,34 @@
+package com.rm_higs.reactive_kafka.sec5;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.kafka.receiver.KafkaReceiver;
+import reactor.kafka.receiver.ReceiverOptions;
+
+import java.util.List;
+import java.util.Map;
+
+public class KafkaConsumer {
+
+	private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
+
+	public static void start(String instanceId) {
+
+		var consumerConfig = Map.<String, Object>of(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
+		                                            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+		                                            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+													ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest",
+													ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, instanceId,
+		                                            ConsumerConfig.GROUP_ID_CONFIG, "demo-group-123");
+		var options = ReceiverOptions.create(consumerConfig)
+		                             .subscription(List.of("order-events"));
+		KafkaReceiver.create(options)
+		             .receive()
+		             .doOnNext(record -> log.info("key: {}, value: {}", record.key(), record.value()))
+		             .doOnNext(record -> record.receiverOffset()
+		                                       .acknowledge())
+		             .subscribe();
+	}
+}
